fc68da06f995c89b9f7d4a3c402966101f04690c
OrionCoy <calvinocoy@gmail.com>
2024-09-06 04:04:07 -0700

    accident!

--- Source/MCPP/src/main/java/mcpp/mcpp/init/CreativeTabInit.java
+++ Source/MCPP/src/main/java/mcpp/mcpp/init/CreativeTabInit.java
@@ -16,10 +16,22 @@ public class CreativeTabInit {
     // Creates the DeferredRegister that is then registered to the bus
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Mcpp.MODID);
 
-    public static final List<Supplier<? extends ItemLike>> MCPP_TAB = new ArrayList<>();
+    public static final List<Supplier<? extends ItemLike>> EXAMPLE_TAB_ITEMS = new ArrayList<>();
+
+    // Creates the DeferredRegister that is then registered to the bus
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = TABS.register("example_tab",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.example_tab"))
+                    .icon(ItemInit.EXAMPLE_ITEM.get()::getDefaultInstance)
+                    .displayItems((displayParams, output) ->
+                            // items are added here
+                            EXAMPLE_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get())
+                            ))
+                    .build()
+    );
 
     public static <T extends ItemLike> RegistryObject<T> addToTab(RegistryObject<T> itemLike) {
-        MCPP_TAB.add(itemLike);
+        EXAMPLE_TAB_ITEMS.add(itemLike);
         return itemLike;
     }
 }
